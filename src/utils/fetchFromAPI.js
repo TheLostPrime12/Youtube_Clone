765f5cb04157b16e8153fdfa1b8999b24ffaa31e
Tanish Garg <83292277+TheLostPrime12@users.noreply.github.com>
2022-09-19 13:00:15 +0530

    Added fetch and Improved UI

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'e58188bdf6msh8032c1c926177e9p1db0a2jsn0e81b3ef509f',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
