94e465c6fd6118d6f670a14e2279f3fc38bb66db
Tanish Garg <83292277+TheLostPrime12@users.noreply.github.com>
2022-10-24 14:47:25 +0530

    Added Channel Page

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,15 +4,22 @@ import { CheckCircle } from '@mui/icons-material';
 import { Link } from 'react-router-dom'
 import { demoProfilePicture } from '../utils/constants';
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
   return (
     <Box
       sx={{
         boxShadow: 'none',
         borderRadius: '20px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: { xs: '356px', md: '310px'},
+        height: '280px',
+        margin: 'auto',
+        marginTop,
       }}
       >
-        <Link to={`/channel/${channelDetail?.id}`}>
+        <Link to={`/channel/${channelDetail?.id?.channelId}`}>
           <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
             <CardMedia 
               image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
